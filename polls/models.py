7e1ed2f8b91558631d2c612b95f4744ab1273af2
John Gutierrez <djterez88@gmail.com>
2021-02-25 10:33:26 -0800

    Learning model realtionships

--- polls/models.py
+++ polls/models.py
@@ -24,3 +24,39 @@ class Choice(models.Model):
     # Will see how this plays out
     # q = Question(question_text="What's new?", pub_date=timezone.now()) 
 
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=20)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
+
+    def __str__(self):
+        return self.name
+
+
+
+'''
+    - This should prob be in views.py:
+        - Microsoft.programmer_set - makes the oop go in reverse - Taking the name of the class(lowercased) and appending _set
+
+        - This line will give you a reverse many-to-one description
+
+    - So when applying:
+        - Microsoft.programmer_set.all():
+            - It will give you [<Programmer: Stacy>, <Programmer: Kelly>]>
+
+
+'''
